5aeb2de1c2e291d71b21ffd336ab78efc0f56ebd
IWON0 <alexey271106@gmail.com>
2024-12-10 11:46:37 +0300

    setting up authorization via spotify

--- src/oauth/endpoint/auth_views.py
+++ src/oauth/endpoint/auth_views.py
@@ -4,12 +4,17 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 
 from .. import serializer
-from ..services.google import check_google_auth
+from ..services import google, spotify
 
 
 def google_login(request):
     """Страница входа через Google"""
     return render(request, 'oauth/google_login.html')
+
+
+def spotify_login(request):
+    """Страница входа через Spotify"""
+    return render(request, 'oauth/spotify_login.html')
 
 
 @api_view(["POST", "OPTIONS"])
@@ -25,7 +30,14 @@ def google_auth(request):
     # Ваш текущий код для обработки POST-запросов:
     google_data = serializer.GoogleAuth(data=request.data)
     if google_data.is_valid():
-        token = check_google_auth(google_data.data)
+        token = google.check_google_auth(google_data.data)
         return Response(token)
     else:
         return AuthenticationFailed(code=403, detail='Bad data Google')
+
+
+@api_view(['GET'])
+def spotify_auth(request):
+    """Подтверждение атворизации через Spotify"""
+    token = spotify.spotify_auth(request.query_params.get('code'))
+    return Response(token)
--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
 
     path('', auth_views.google_login),
-    path('google/', auth_views.google_auth)
+    path('google/', auth_views.google_auth),
+    path('spotify-login/', auth_views.spotify_login),
+    path('spotify-callback/', auth_views.spotify_auth)
 ]
