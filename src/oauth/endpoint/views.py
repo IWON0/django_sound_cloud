8c5951cbc787386966f648ee31a2c51b85c85fcb
IWON0 <alexey271106@gmail.com>
2024-12-10 23:53:20 +0300

    creating serializer and permissions

--- src/oauth/endpoint/views.py
+++ src/oauth/endpoint/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, parsers, permissions
 
 from .. import serializer, models
+from ...base.permissions import IsAuthor
 
 
 class UserView(viewsets.ModelViewSet):
@@ -18,5 +19,17 @@ class UserView(viewsets.ModelViewSet):
 
 class AuthorView(viewsets.ReadOnlyModelViewSet):
     """list of authors"""
-    queryset = models.AuthUser.objects.all()
+    queryset = models.AuthUser.objects.all().prefetch_related('social_links')
     serializer_class = serializer.AuthorSerializer
+
+
+class SocialLinkView(viewsets.ModelViewSet):
+    """CRUD of user social network links"""
+    serializer_class = serializer.SocialLinkSerializer
+    permission_classes = [IsAuthor]
+
+    def get_queryset(self):
+        return self.request.user.social_links.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
